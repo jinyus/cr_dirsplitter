020e2cbdb0bda5bd4be13ae686f926fb668d2751
jinyus <starboymc4@gmail.com>
2021-12-21 00:29:12 -0500

    first commit

---
+++ src/dirsplitter.cr
@@ -0,0 +1,49 @@
+require "option_parser"
+require "./*"
+
+split_mode = false
+reverse_mode = false
+dir = "."
+max = 5.0
+prefix = ""
+
+parser = OptionParser.new do |parser|
+  parser.banner = "Usage: dirsplitter [subcommand] [arguments] [DIRECTORY]"
+
+  parser.on("split", "Splits directories into a specified maximum size") do
+    split_mode = true
+    parser.banner = "Usage: displitter split [arguments] [FOLDER]"
+    parser.on("-d DIRPATH", "--dir=DIRPATH", DIR_DESC) { |_dir| dir = _dir }
+    parser.on("-m MAX", "--max=MAX", MAX_DESC) { |_max| max = _max.to_f }
+    parser.on("-p PREFIX", "--prefix=PREFIX", PREFIX_DESC) { |pre| prefix = pre }
+  end
+
+  parser.on("reverse", REVERSE_DESC) do
+    reverse_mode = true
+    parser.banner = "Usage: displitter reverse [DIRECTORY]"
+    parser.on("-d DIRPATH", "--dir=DIRPATH", DIR_DESC) { |_dir| dir = _dir }
+  end
+
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+parser.parse
+
+if split_mode
+  split_dir(dir, (max * GBMULTIPLE).to_u64, prefix)
+elsif reverse_mode
+  reverse_split(dir)
+else
+  STDERR.puts "Error parsing args"
+  puts parser
+  exit(1)
+end
---
+++ src/split.cr
@@ -0,0 +1,72 @@
+require "walk"
+
+def split_dir(dir : String, max_bytes : UInt64, prefix : String)
+  tracker = Hash(Int32, UInt64).new
+  current_part = 1
+  files_moved = 0
+  failed_ops = 0
+  base_dir_path = Path.new(dir)
+
+  Walk::Down.new(dir).each do |path|
+    if !File.file?(path)
+      next
+    end
+
+    size = File.size(path)
+
+    decrement_if_failed = false
+
+    current_part_size = tracker.fetch(current_part, 0)
+
+    if current_part_size > 0 && current_part_size + size > max_bytes
+      current_part += 1
+      decrement_if_failed = true
+    end
+
+    tracker[current_part] = (tracker.has_key?(current_part) ? current_part_size + size : size).to_u64
+
+    part_dir = base_dir_path.join("part#{current_part}")
+
+    new_path = Path.new(path.expand.to_s.sub(
+      base_dir_path.expand.to_s,
+      part_dir.expand.to_s
+    ))
+
+    begin
+      if !Dir.exists?(new_path.parent)
+        Dir.mkdir_p(new_path.parent)
+      end
+
+      File.rename(
+        path.expand.to_s,
+        new_path.expand.to_s
+      )
+
+      files_moved += 1
+    rescue exception
+      STDERR.puts exception
+      failed_ops += 1
+      tracker[current_part] -= size
+
+      if decrement_if_failed
+        current_part -= 1
+      end
+    end
+  end
+
+  if files_moved == 0
+    current_part = 0
+  end
+
+  puts "Parts created: #{current_part}"
+  puts "Files moved: #{files_moved}"
+  puts "Failed Operations: #{failed_ops}"
+
+  if current_part > 0 && !prefix.blank?
+    if current_part == 1
+      puts %(Tar Command : tar -cf "#{prefix}part1.tar" "part1"; done)
+    else
+      puts %(Tar Command : for n in {{1..#{current_part}}}; do tar -cf "#{prefix}part$n.tar" "part$n"; done)
+    end
+  end
+end
